626036df702ac133285334a336d41de2bd009f7b
nirav-chavda <niravdchavda@gmail.com>
2018-07-16 17:59:02 +0530

    Initial Commit

---
+++ server.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+// register partials directory
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine','hbs');
+
+// registering middleware to log the request detials
+app.use( (req,res,next) => {
+  var data = `${ new Date().toString() } : ${ req.method } ${ req.url }\n`;
+  fs.appendFile( __dirname+'/logcat.txt' , data , (error) => { if(error) { console.log(error) } });
+  next();
+});
+
+// Maintanance mode
+// app.use((req,res,next) => {
+//   res.render('maintanance.hbs');
+// });
+
+// registering middleware for static directory
+app.use(express.static( __dirname + '/public' ));
+
+
+// registering helper function
+hbs.registerHelper('getCurrentYear' , () => {
+  return new Date().getFullYear()
+});
+
+hbs.registerHelper('capitalizeIt',(text1,text2) => {
+  return text1.toUpperCase() + " " +text2.toUpperCase() ;
+});
+
+app.get('/', (req,res)=> {
+  res.render('index.hbs',{
+    'name' : 'Nirav'
+  });
+});
+
+app.get('/json_data', (req,res) => {
+  res.send({
+      name : 'nirav',
+      likes : [
+        'Biking',
+        'Gaming'
+      ]
+  });
+});
+
+app.get('/about' , (req,res) => {
+  res.render('about.hbs',{
+    'title' : "About"
+  });
+});
+
+app.get('/bad', (req,res) => {
+  res.send({
+    error : 'Unable to handle the request'
+  });
+});
+
+app.listen(3000, () => { console.log('server started on port number 3000'); });
